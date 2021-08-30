3ec1f8a0e32fe2a33f55439b565a7a6a1d4986d9
Adam Faidy <Faidyadam1@gmail.com>
2021-08-29 23:15:08 -0400

    Alternate Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -24,3 +24,46 @@ const Footer = () => {
   );
 };
 export default Footer;
+
+// Alternate Footer
+
+// import React from "react";
+// import "./Navigation.css";
+
+// import Helios from "../images/Helios.png";
+// function Footer() {
+//   return (
+//     <div className="footer-div">
+//       <div className="footer-wrapper">
+//         <h1 className="footer-logo" href="/">
+//           Hyperion
+//         </h1>
+//         <ul className="footer-ul">
+//           <li>
+//             <i class="fas fa-home icon2"></i>
+//             <a href="/" className="footer-a">
+//               Home
+//             </a>
+//           </li>
+//           <li>
+//             <i class="fab fa-github icon2"></i>
+//             <a href="https://github.com/Sisysphus" className="footer-a">
+//               Github
+//             </a>
+//           </li>
+//           <li>
+//             <i class="fab fa-linkedin icon2"></i>
+//             <a
+//               href="https://www.linkedin.com/in/adam-faidy-bb8784105/"
+//               className="footer-a"
+//             >
+//               LinkedIn
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Footer;
