6e4d6dffbc7c680e2c87d6d3c6fb3a80e8de841d
Adam Faidy <Faidyadam1@gmail.com>
2021-09-02 23:26:08 -0400

    Starting replicate code [ Pre-custom ]

---
+++ frontend/src/components/AWS/Main.js
@@ -0,0 +1,39 @@
+import React from "react";
+import LeftNavPanel from "./LeftNavPanel";
+import RightPanel from "./RightPanel/RightPanel";
+import "./Main.css";
+import { Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "../../store/ducks/users";
+import RightHomePanel from "./RightPanel/RightHomePanel";
+
+const Main = () => {
+  const deletedId = useSelector((state) => state.users.trashBinId);
+  const token = useSelector((state) => state.authentication);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  return (
+    <div className="mainContainer">
+      <div className="leftNavPanel">
+        <LeftNavPanel />
+      </div>
+      <Route path="/home">
+        <div className="rightPanel">
+          <RightHomePanel token={token} />
+        </div>
+      </Route>
+      <Route path="/folders/:id">
+        <div className="rightPanel">
+          <RightPanel deletedId={deletedId} />
+        </div>
+      </Route>
+    </div>
+  );
+};
+
+export default Main;
